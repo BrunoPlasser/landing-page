bee0f05707d41492c868cf8b11582bc2dcb16472
BrunoPlasser <148269476+BrunoPlasser@users.noreply.github.com>
2024-04-29 23:22:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+document.getElementById("repositorioButton").addEventListener("click", Rb);
+
+function Rb() {
+    window.location.href = "https://github.com/BrunoPlasser?tab=repositories";
+}
+
+document.getElementById("RoadSitebt").addEventListener("click", PS);
+
+function PS() {
+    window.location.href = "https://brunoplasser.github.io/ping-pong/";
+}
+
+document.getElementById("QuizSitebt").addEventListener("click", QS);
+
+function QS() {
+    window.location.href = "https://brunoplasser.github.io/quiz-the-road/";
+}
+
+document.getElementById("RoadSitebt").addEventListener("click", RS);
+
+function RS() {
+    window.location.href = "https://brunoplasser.github.io/pagina-the-road/";
+}
